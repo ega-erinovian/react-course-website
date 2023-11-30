414f3fc214c4afa25646a13a7cfb68ea4adf0478
ega-erinovian <erinovian69@gmail.com>
2023-11-30 14:48:12 +0700

    TermsCond Page DONE

--- src/pages/TermCondPage.jsx
+++ src/pages/TermCondPage.jsx
@@ -1,6 +1,49 @@
+import { Container, Row, Col } from "react-bootstrap";
+import FaQComponent from "../components/FaQComponent";
+
 const TermCondPage = () => {
   return (
-    <div>TermCondPage</div>
+    <div className="termscond-page">
+      <div className="termscond min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center mb-2">Syarat & Ketentuan</h1>
+              <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum animi natus sequi repellat corrupti iusto officia eius libero ut tempora totam, iure quis vero placeat odio, reprehenderit, architecto sit! Maiores, eius praesentium, officia tempora consectetur iste recusandae accusamus harum blanditiis repellendus assumenda fuga. Amet culpa eaque harum incidunt temporibus.</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis autem quidem animi ex saepe sed, itaque, ullam temporibus cupiditate nemo commodi explicabo voluptates voluptas enim eum ab rerum? Porro sapiente, velit dolor iste sit a inventore beatae autem minus officia!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nostrum, ea illum delectus voluptatem facilis laboriosam odit quaerat possimus optio quas sed quasi id dolor expedita tempore, in corrupti aspernatur obcaecati officiis eos numquam voluptates amet! Quam id ratione repudiandae adipisci facere, inventore minima recusandae delectus, praesentium culpa eos eligendi, ullam ad consequuntur laboriosam eaque enim! Ab doloremque ipsum facere?</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, laudantium modi, veritatis perspiciatis iusto aspernatur saepe vero adipisci ducimus consectetur totam in neque aut asperiores voluptate eligendi molestiae, amet at minus! Sint dolorum sapiente rem, similique magnam veritatis magni iusto.</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis autem quidem animi ex saepe sed, itaque, ullam temporibus cupiditate nemo commodi explicabo voluptates voluptas enim eum ab rerum? Porro sapiente, velit dolor iste sit a inventore beatae autem minus officia!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, laudantium modi, veritatis perspiciatis iusto aspernatur saepe vero adipisci ducimus consectetur totam in neque aut asperiores voluptate eligendi molestiae, amet at minus! Sint dolorum sapiente rem, similique magnam veritatis magni iusto.</p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Dolor Sit</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis autem quidem animi ex saepe sed, itaque, ullam temporibus cupiditate nemo commodi explicabo voluptates voluptas enim eum ab rerum? Porro sapiente, velit dolor iste sit a inventore beatae autem minus officia!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, laudantium modi, veritatis perspiciatis iusto aspernatur saepe vero adipisci ducimus consectetur totam in neque aut asperiores voluptate eligendi molestiae, amet at minus! Sint dolorum sapiente rem, similique magnam veritatis magni iusto.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consequuntur culpa aliquid similique deserunt, totam mollitia iusto perspiciatis, sapiente, ratione doloremque maxime ullam rem neque unde saepe impedit vero atque! Distinctio asperiores minus fugit hic corporis expedita earum voluptas ab eos laboriosam. Voluptates laudantium reprehenderit rem eius. Facere voluptate nam illo iste corrupti exercitationem, ducimus, reiciendis accusantium similique voluptatem assumenda?</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <FaQComponent/>
+    </div>
   )
 }
 
